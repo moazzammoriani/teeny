321af97e1582564db2f46d6aa75a2c33b17534e8
Moazzam Moriani <mozmoriani@gmail.com>
2023-07-20 17:02:02 +0500

    Initial commit

---
+++ db_schema.sql
@@ -0,0 +1,51 @@
+
+-- This makes sure that foreign_key constraints are observed and that errors will be thrown for violations
+PRAGMA foreign_keys=ON;
+
+BEGIN TRANSACTION;
+
+--create your tables with SQL commands here (watch out for slight syntactical differences with SQLite)
+
+CREATE TABLE IF NOT EXISTS testUsers (
+    test_user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    test_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS testUserRecords (
+    test_record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    test_record_value TEXT NOT NULL,
+    test_user_id  INT, --the user that the record belongs to
+    FOREIGN KEY (test_user_id) REFERENCES testUsers(test_user_id)
+);
+
+--insert default data (if necessary here)
+
+INSERT INTO testUsers ('test_name') VALUES ('Simon Star');
+INSERT INTO testUserRecords ('test_record_value', 'test_user_id') VALUES ('Lorem ipsum dolor sit amet', 1); --try changing the test_user_id to a different number and you will get an error
+
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    name TEXT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS blogs (
+    blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    subtitle TEXT NOT NULL,
+    author INT,
+    FOREIGN KEY (author) REFERENCES users(user_id)
+);
+
+
+INSERT INTO users ('username', 'name') VALUES ('mwzm', 'Moazzam Moriani');
+INSERT INTO users ('username', 'name') VALUES ('yeeking', 'Mathew Yee-king');
+
+INSERT INTO blogs ('title', 'subtitle', 'author') VALUES ('I like coding in functional languages', 'a true story', 1);
+INSERT INTO blogs ('title', 'subtitle', 'author') VALUES ('I like coding in C', 'oof', 1);
+INSERT INTO blogs ('title', 'subtitle', 'author') VALUES ('I like coding pasta', 'fax', 1);
+
+COMMIT;
+
